8c2d298f4cc65aaaf132a01cb438967ab3786cda
Jeroen Valcke <jeroen.valcke@studiohyperdrive.be>
2022-03-28 18:24:51 +0200

    feat(reindex): add modal

--- public/lib/services/search/search.service.ts
+++ public/lib/services/search/search.service.ts
@@ -3,8 +3,8 @@ import api from '../api/api.service';
 import { SITE_REQUEST_PREFIX_URL } from './search.service.const';
 
 export class SearchApiService {
-	public triggerReindex(siteId: string): Promise<void> {
-		return api.post(`${SITE_REQUEST_PREFIX_URL}/${siteId}/content/reindex`).json();
+	public async triggerReindex(siteId: string): Promise<void> {
+		await api.post(`${SITE_REQUEST_PREFIX_URL}/${siteId}/content/reindex`);
 	}
 }
 
